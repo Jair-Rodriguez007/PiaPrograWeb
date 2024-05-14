01f1d0a9361868b9ab2c2b12b1e8b8e7b1e49f19
unknown <jair123potrillo@gmail.com>
2024-05-14 11:08:50 -0600

    Agregué el código js para la pantalla 4 que es la del carrito

---
+++ js/pantalla4.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    var params = new URLSearchParams(window.location.search);
+    var id = params.get('idLibro');
+    console.log("Esta es la variable params: "+params);
+    //console.log("Esta es la variable id: "+id);
+    $.ajax({
+        url: '/PiaPrograWeb/php/getLibros.php',
+        type: 'GET',
+        data: { id: id },
+        dataType: 'json',
+        success: function(libro) {
+            console.log(libro);
+            $('#titulo-libro').text(libro.Titulo);
+            $('#descripcion-libro').text(libro.Descripcion);
+            $('#precio-libro').text('Precio: $' + libro.Precio);
+            if(libro.CantidadInventario > 0) {
+                $('#cantidad-libro').text('Estado: En existencia');
+            } else {
+                $('#cantidad-libro').text('Estado: Agotado');
+            }
+            $('#categoria-libro').text('Categoría: ' + libro.NombreCategoria);
+            $('#imagen-libro').attr('src', libro.imagen);
+        },
+        error: function(error) {
+            console.log(error);
+            alert('Hubo un error al cargar los detalles del libro. Por favor, inténtalo de nuevo más tarde.');
+        }
+    });
+
+    $('#btnCarrito').click(function() {
+    var idLibro = params.get('idLibro');
+    var idUsuario = params.get('idUsuario');
+    var cantidad = $('#quantity').val();
+    console.log("Esta es la variable idLibro: "+idLibro);
+    console.log("Esta es la variable idUsuario: "+idUsuario);
+    console.log("Esta es la variable cantidad: "+cantidad);
+    $.ajax({
+        url: 'agregar-carrito.php',
+        type: 'POST',
+        data: { 
+            idLibro: idLibro,
+            idUsuario: idUsuario,
+            cantidad: cantidad
+        },
+        success: function(response) {
+            alert('Libro añadido al carrito exitosamente');
+        },
+        error: function(error) {
+            console.log(error);
+            alert('Hubo un error al añadir el libro al carrito. Por favor, inténtalo de nuevo más tarde.');
+        }
+    });
+});
+});
