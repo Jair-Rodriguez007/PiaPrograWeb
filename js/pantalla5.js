3d8edbfa290838a69a163ce2f4dd418410ea8662
unknown <jair123potrillo@gmail.com>
2024-05-15 18:40:13 -0600

    Agregué el php y js para traer las tarjetas del usuario

--- js/pantalla5.js
+++ js/pantalla5.js
@@ -14,6 +14,23 @@ function traerDomicilio() {
     });
 }
 
+function traerTarjeta() {
+    $.ajax({
+        url:"/PiaPrograWeb/php/getTarjetas.php",
+        type:'GET',
+        dataType:'json',
+        crossDomain:true
+    }).done(function(result) {
+        $(result).each(function(index) {
+            $("#payment-method").append(`<option value="${this.IdTarjeta}">${this.NumeroTarjeta}</option>`);
+        });
+        console.log(result);
+    }).fail(function(xhr, status, error) {
+        alert(error)
+    });
+}
+
 $(document).ready(function() {
     traerDomicilio();
+    traerTarjeta();
 });
