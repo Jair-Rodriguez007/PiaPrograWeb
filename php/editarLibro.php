b172718e9b670ef38d8e21f4b55c36d5b84a6926
unknown <jair123potrillo@gmail.com>
2024-05-18 12:13:10 -0600

    Agregué el código para cargar documentos

--- php/editarLibro.php
+++ php/editarLibro.php
@@ -1,28 +1,45 @@
 <?php
 require_once 'config.php';
 
-if(isset($_POST)){
-    $id = $_POST['bookId'];  // Asegúrate de que este valor se está enviando correctamente
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = intval($_POST['bookId']);  // Asegúrate de que este valor se está enviando correctamente
     $nombre = $_POST['editName'];
     $descripcion = $_POST['editDescription'];
     $precio = $_POST['editPrice'];
     $categoria = intval($_POST['editCategory']);
     $cantidad = $_POST['editQuantity'];
 
-    echo gettype($categoria);
-    echo 'El id del libro es: '.$id;
-
     $conexion = conexionDb::conexion();
     if ($conexion->connect_error) {
         die("Connection failed: " . $conexion->connect_error);
     }
 
-    $sql = "UPDATE Producto SET Titulo = '$nombre', Descripcion = '$descripcion', CategoriaPrincipal = $categoria, Precio = $precio, CantidadInventario = $cantidad WHERE IdProducto = $id";
+    // Manejar la subida de la imagen
+    if ($_FILES['editImage']['error'] === UPLOAD_ERR_OK) {
+        $imageTmpPath = $_FILES['editImage']['tmp_name'];
+        $imageName = basename($_FILES['editImage']['name']);
+        $imageDir = 'C:/xampp/htdocs/PiaPrograWeb/img/'; // Asegúrate de actualizar esta ruta
+        $imagePath = $imageDir . $imageName;
+
+        // Mueve el archivo subido a la ubicación deseada
+        if (move_uploaded_file($imageTmpPath, $imagePath)) {
+            // Actualiza el registro del libro con la nueva imagen
+            $sql = "UPDATE Producto SET Titulo = '$nombre', Descripcion = '$descripcion', CategoriaPrincipal = $categoria, Precio = $precio, CantidadInventario = $cantidad, Imagen = '$imagePath' WHERE IdProducto = $id";
+        } else {
+            echo "Error al mover el archivo subido.";
+            exit();
+        }
+    } else {
+        // Si no se sube una nueva imagen, no actualiza el campo de la imagen
+        $sql = "UPDATE Producto SET Titulo = '$nombre', Descripcion = '$descripcion', CategoriaPrincipal = $categoria, Precio = $precio, CantidadInventario = $cantidad WHERE IdProducto = $id";
+    }
+
     if ($conexion->query($sql) === TRUE) {
         echo "Record updated successfully";
     } else {
         echo "Error: " . $sql . "<br>" . $conexion->error;
     }
     header('Location: /PiaPrograWeb/html/pantallaAdmin4.html');
+    exit();
 }
 ?>
