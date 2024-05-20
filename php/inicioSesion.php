4cc271a58f433b2eb03b3bc7ab761bb59853e68d
unknown <jair123potrillo@gmail.com>
2024-05-20 13:52:16 -0600

    Mofiqué la lógica para iniciar sesión

--- php/inicioSesion.php
+++ php/inicioSesion.php
@@ -11,17 +11,25 @@ if(isset($_POST)){
       die("Connection failed: " . $conexion->connect_error);
   }
 
-  $sql = "SELECT IdUsuario FROM Usuario WHERE CorreoElectronico='$email' AND Contrasena='$password'";
+  // Modificar la consulta SQL para obtener también el Rol del usuario
+  $sql = "SELECT IdUsuario, Rol FROM Usuario WHERE CorreoElectronico='$email' AND Contrasena='$password'";
   $result = $conexion->query($sql);
 
   if ($result->num_rows > 0) {
     // El usuario existe, iniciar sesión
     session_start();
     $_SESSION['loggedin'] = true;
-    // Obtener el ID_usuario
+    // Obtener el ID_usuario y el Rol
     $row = $result->fetch_assoc();
     $_SESSION['id'] = $row['IdUsuario'];
-    header("Location: /PiaPrograWeb/html/pantalla3.html"); // Redirigir a la siguiente página
+    $_SESSION['rol'] = $row['Rol'];
+
+    // Redirigir a la página correspondiente según el Rol
+    if ($_SESSION['rol'] == 2) {
+        header("Location: /PiaPrograWeb/html/pantalla3.html");
+    } else {
+        header("Location: /PiaPrograWeb/html/pantallaAdmin1.html");
+    }
   } else {
     // El usuario no existe
     echo "Usuario o contraseña incorrectos";
